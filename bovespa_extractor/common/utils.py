c70aa216fbca4f4f8dd8d58883ae3ffac2ce862b
Matheus Felipe dos Anjos <mfa@smartbank.com.br>
2020-04-03 20:04:24 -0300

    Extraction working and saving file in local storag

--- bovespa_extractor/common/utils.py
+++ bovespa_extractor/common/utils.py
@@ -1,5 +1,5 @@
 import json
-import time
+import os
 from datetime import datetime
 
 def load_file_json(file_name: str) -> dict:
@@ -10,5 +10,10 @@ def load_file_url(file_name: str) -> str:
     with open(file_name, 'r') as f:
         return f.read()
 
-def str_date_to_timestamp(date: str, date_format: str = "%Y-%m-%d") -> int:
+def str_date_to_timestamp(date: str, date_format: str="%Y-%m-%d") -> int:
     return int(datetime.timestamp(datetime.strptime(date, date_format)))
+
+def create_dir(file_path: str) -> None:
+    path = os.path.dirname(file_path)
+    if not os.path.exists(path):
+        os.makedirs(path)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from bovespa_extractor.common.utils import load_file_json, load_file_url, str_date_to_timestamp
+from bovespa_extractor.common.utils import load_file_json, load_file_url, str_date_to_timestamp, create_dir
+
+import requests
+import csv
 
 CONFIG_MAIN_FILE = "./configuration/extraction_main.json"
 CONFIG_STOCK_FILE = "./configuration/extraction_stocks.json"
-URL_FILE = "url"
+URL_FILE = "./configuration/url"
+ENCODING = "UTF-8"
+DATA_DIR = "./data/"
+
+create_dir(DATA_DIR)
 
 configs_main = load_file_json(CONFIG_MAIN_FILE)
 configs_stock = load_file_json(CONFIG_STOCK_FILE)
@@ -13,8 +20,24 @@ date_end = configs_main.get("DATE_END")
 def load_stocks_to_extract(file_name: str) -> list:
     return configs_main.get("EXTRACT_STOCKS")
 
+def get_stock_data(stock: str) -> requests.models.Response:
+    return requests.get(url.format(stock=stock, date_start=date_start_ts, date_end=date_end_ts))
 
+def write_into_csv(data: requests.models.Response, file_name: str) -> None:
+    with open(DATA_DIR+"{name}.csv".format(name=file_name), "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(csv.reader(data.content.decode(ENCODING).splitlines(), delimiter=','))
+
+def run_extraction_stock(stocks):
+    for stock in stocks:
+        data = get_stock_data(stock)
+        write_into_csv(data, stock)
 
 if __name__ == "__main__":
     stocks = load_stocks_to_extract(CONFIG_MAIN_FILE)
     url = load_file_url(URL_FILE)
+
+    date_start_ts = str_date_to_timestamp(date_start)
+    date_end_ts = str_date_to_timestamp(date_end)
+
+    run_extraction_stock(stocks)
