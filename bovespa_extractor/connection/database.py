5a7994d3d136fc02d5fbbf1ac315e6674b3e6e72
Matheus Felipe dos Anjos <mfa@smartbank.com.br>
2020-04-04 00:30:25 -0300

    Module database for Postgres connection added

---
+++ bovespa_extractor/connection/database.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+class Postgres:
+
+    def __init__(self, host, user, password, port, database):
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__port = port
+        self.__database = database
+
+        self.__create_connection()
+
+    def __create_connection(self):
+        self.__conn = psycopg2.connect(
+            user=self.__user,
+            password=self.__password,
+            host=self.__host,
+            port=self.__port,
+            database=self.__database
+        )
+
+    @property
+    def get_cursor(self):
+        return self.__conn.cursor
